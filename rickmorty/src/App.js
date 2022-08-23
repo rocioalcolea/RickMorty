d9007a9524e8a58bacb4577c6242b6b62fd06f00
rocio <rocio@rocioalcolea>
2022-08-23 10:28:42 +0200

    rickMorty formateado

---
+++ rickmorty/src/App.js
@@ -0,0 +1,35 @@
+import "./App.css";
+import imageRickMorty from "./img/rick-morty.png";
+import { useState } from "react";
+import Characters from "./components/Character";
+
+function App() {
+  const [characters, setCharacters] = useState(null);
+  const reqApi = async () => {
+    const api = await fetch("https://rickandmortyapi.com/api/character/");
+    //console.log(api);
+    const characterApi = await api.json();
+    //console.log(characterApi);
+    setCharacters(characterApi.results);
+  };
+
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1 className="title">Rick & Morty</h1>
+        {characters ? (
+          <Characters characters={characters} setCharacters={setCharacters} />
+        ) : (
+          <>
+            <img src={imageRickMorty} alt="Rick & Morty" className="img-home" />
+            <button onClick={reqApi} className="btn-search">
+              Buscar personajes
+            </button>
+          </>
+        )}
+      </header>
+    </div>
+  );
+}
+
+export default App;
